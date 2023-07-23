8740ca2ad978bc61bd06ae0f975cf669e5cb09b8
weitnow <weitnow@gmail.com>
2023-07-23 20:41:44 +0200

    check_collisions added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+MOVE_INCREMENT = 20
+MOVES_PER_SECOND = 15
+GAME_SPEED = 1000 // MOVES_PER_SECOND
+
 class Snake(tk.Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
 
         self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+        self.food_position = (200, 100)
+        self.score = 0
 
         self.load_assets()
         self.create_objects()
+
+        self.after(GAME_SPEED, self.perform_actions)
+        #self.perform_actions()
 
     def load_assets(self):
         try:
@@ -22,9 +31,33 @@ class Snake(tk.Canvas):
             root.destroy()
 
     def create_objects(self):
+        self.create_text(45, 12, text=f"Score: {self.score}", tag="score", fill="#fff", font=("TkDefaultFont", 14))
         for x_position, y_position in self.snake_positions:
             self.create_image(x_position, y_position, image=self.snake_body, tag="snake")
 
+        self.create_image(*self.food_position, image=self.food, tag="food")
+        self.create_rectangle(7, 28, 593, 613, outline="#fff")
+
+    def move_snake(self):
+        head_x_position, head_y_position = self.snake_positions[0]
+        new_head_position = (head_x_position + MOVE_INCREMENT, head_y_position)
+        self.snake_positions = [new_head_position] + self.snake_positions[:-1]
+
+        for segment, position in zip(self.find_withtag("snake"), self.snake_positions):
+            self.coords(segment, position)
+
+    def perform_actions(self):
+        self.move_snake()
+        self.after(GAME_SPEED, self.perform_actions)
+
+    def check_collisions(self):
+        head_x_position, head_y_position = self.snake_positions[0]
+
+        return (
+            head_x_position in (0, 600)
+            or head_y_position in (20, 620)
+            or (head_x_position, head_y_position) in self.snake_positions[1:]
+        )
 
 root = tk.Tk()
 root.title("Snake")
