530498a0788c03ef6de61e975d5b1b6d01edb224
Christian Keusch <weitnow@gmail.com>
2023-07-23 10:04:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class Snake(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, background="black", highlightthickness=0)
+
+        self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+
+        self.load_assets()
+        self.create_objects()
+
+    def load_assets(self):
+        try:
+            self.snake_body_image = Image.open("./assets/snake.png")
+            self.sanke_body = ImageTk.PhotoImage(self.snake_body_image)
+
+            self.food_image = Image.open("./assets/food.png")
+            self.food = ImageTk.PhotoImage(self.food_image)
+        except IOError as error:
+            print(error)
+            root.destroy()
+
+    def create_asstes(self):
+
+
+root = tk.Tk()
+root.title("Snake")
+root.resizable(False, False)
+
+board = Snake()
+board.pack()
+
+root.mainloop()
